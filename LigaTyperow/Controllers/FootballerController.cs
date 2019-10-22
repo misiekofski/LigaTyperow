1a5b86995523ddd1c5cf52ecf275f706ea31e518
Michal Dobrzycki <dobrzycki@gmail.com>
2019-10-22 21:56:12 +0200

    Added proper controllers

---
+++ LigaTyperow/Controllers/FootballerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LigaTyperow.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LigaTyperow.Controllers
+{
+    [Route("api/footballers")]
+    [ApiController]
+    public class FootballerController : ControllerBase
+    {
+        private readonly ILogger<FootballerController> _logger;
+
+        public FootballerController(ILogger<FootballerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Footballer> Get() => new List<Footballer>();
+    }
+}
---
+++ LigaTyperow/Controllers/TeamController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LigaTyperow.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LigaTyperow.Controllers
+{
+    [Route("api/teams")]
+    [ApiController]
+    public class TeamController : ControllerBase
+    {
+        private readonly ILogger<MatchController> _logger;
+
+        public TeamController(ILogger<MatchController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Team> Get() => new List<Team>();
+    }
+}
