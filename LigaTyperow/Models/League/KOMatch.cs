2f65db68edd2aec0874f566369c6add8b529bd75
Michal Dobrzycki <dobrzycki@gmail.com>
2019-11-01 12:47:10 +0100

    Added new matches and tree class

---
+++ LigaTyperow/Models/League/KOMatch.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LigaTyperow.Models.League
+{
+    public class KOMatch : Match
+    {
+        private Team WinnerTeam { get; set; }
+    }
+}
---
+++ LigaTyperow/Models/League/KOTree.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace LigaTyperow.Models.League
+{
+    public class KOTree
+    {
+        public List<KOMatch> KOMatches { get; set; }
+        
+        public Team? ThirdPlace { get; set; }
+        public Team SecondPlace { get; set; }
+        public Team Winner { get; set; }
+    }
+}
